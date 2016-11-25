f56b2317db5ab77b02c394c22c0c77abb118b264
Darren Otgaar <darren.otgaar@icloud.com>
2016-11-25 07:10:17 +0200

    - Added new submodules

---
+++ directory_stream.cpp
@@ -0,0 +1,2 @@
+/* Created by Darren Otgaar on 2016/11/24. http://www.github.com/otgaard/zap */
+#include "directory_stream.hpp"
---
+++ directory_stream.hpp
@@ -0,0 +1,12 @@
+/* Created by Darren Otgaar on 2016/11/24. http://www.github.com/otgaard/zap */
+#ifndef ZAPPLAYER_DIRECTORY_STREAM_HPP
+#define ZAPPLAYER_DIRECTORY_STREAM_HPP
+
+
+
+class directory_stream {
+
+};
+
+
+#endif //ZAPPLAYER_DIRECTORY_STREAM_HPP
